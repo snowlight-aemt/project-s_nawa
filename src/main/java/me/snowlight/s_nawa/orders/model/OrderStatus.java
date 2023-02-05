afd0969f1d0ac130cd8a420ef31e01f902f9937b
snowlight-aemt <it.s.aemt@gmail.com>
2023-02-05 13:18:44 +0900

    대략적인 요구사항을 readme에 작성

---
+++ src/main/java/me/snowlight/s_nawa/orders/model/OrderStatus.java
@@ -0,0 +1,8 @@
+package me.snowlight.s_nawa.orders.model;
+
+public enum OrderStatus {
+    PENDING,
+    AWAIT_PAYMENT,
+    AWAIT_SHIPMENT,
+    COMPLETED
+}
---
+++ src/main/java/me/snowlight/s_nawa/orders/model/Orders.java
@@ -0,0 +1,11 @@
+package me.snowlight.s_nawa.orders.model;
+
+
+public class Orders {
+    private Long id;
+
+    private Long shopId;
+    private Long userId;
+    private Long itemId;
+    private OrderStatus status;
+}
