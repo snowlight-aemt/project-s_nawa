df74becb104fe2a053ef389bc11b3c2af42296a9
snowlight-aemt <it.s.aemt@gmail.com>
2023-02-05 11:02:12 +0900

    Init Project

---
+++ src/main/java/me/snowlight/s_nawa/SNawaApplication.java
@@ -0,0 +1,13 @@
+package me.snowlight.s_nawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SNawaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SNawaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/snowlight/s_nawa/SNawaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.snowlight.s_nawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SNawaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
